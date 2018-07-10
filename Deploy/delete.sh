8165073a6fda7e821961e10b49969bb069e28314
Vishnu Ittiyamparampath <ivnavin@hotmail.com>
2018-07-10 23:53:32 +0530

    deployment steps added to readme

--- Deploy/delete.sh
+++ Deploy/delete.sh
@@ -1,4 +1,4 @@
-kubectl delete deployment,service -l app=wordpress
+kubectl delete deployment,service -l app=mlbrFtPrnts
 kubectl delete secret mysql-pass
-kubectl delete pvc -l app=wordpress
+kubectl delete pvc -l app=mlbrFtPrnts
 kubectl delete pv local-pv-1 local-pv-2
--- readme.txt
+++ readme.txt
@@ -1,1 +1,5 @@
 Malabar Footprints Web Site
+
+
+To deploy Malabar Footprints on Minikube run the following command
+cd Deploy && sh ./deploy.sh
